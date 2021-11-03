d5c1bfdeb9dd5b8cd3433652e7294e7f1224de77
meri-xx <meri.leeworthy@gmail.com>
2021-11-04 09:15:23 +1100

    links and visual fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +10,13 @@ const Home: NextPage = () => {
         <meta name="description" content="Meri Leeworthy, internet edition." />
       </Head>
       <main className="flex items-center justify-center w-screen h-screen">
-        <h1 className="font-bold text-9xl font-title">meri.garden</h1>
+        <Link href="blog">
+          <a>
+            <h1 className="text-5xl font-bold sm:text-7xl md:text-8xl lg:text-9xl font-title">
+              meri.garden
+            </h1>
+          </a>
+        </Link>
       </main>
     </div>
   );
