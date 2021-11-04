4e278a2ca0f7adb060368e84c65fb0e15e8268d3
meri-xx <meri.leeworthy@gmail.com>
2021-11-05 09:44:31 +1100

    blog posts

---
+++ pages/[slug].tsx
@@ -0,0 +1,92 @@
+import type { NextPage, GetStaticProps, GetStaticPaths } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { gql } from "@apollo/client";
+import client from "lib/apollo-client";
+import {
+  DocumentRenderer,
+  DocumentRendererProps
+} from "@keystone-next/document-renderer";
+import { Back } from "components/Back";
+
+const GET_PAGE = gql`
+  query ($slug: String) {
+    post(where: { slug: $slug }) {
+      title
+      content {
+        document
+      }
+    }
+  }
+`;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      // static generate these post pages at build time
+    ],
+    // if the page wasn't pre-generated, force the user to wait for the server to generate it
+    fallback: "blocking"
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  try {
+    const { data, error } = await client.query({
+      query: GET_PAGE,
+      variables: {
+        // plug the route into the query
+        slug: params?.slug
+      }
+    });
+    // error -> 404 (rather than just breaking)
+    if (!data || error) return { notFound: true };
+    console.log(data);
+    return {
+      props: {
+        post: data.post
+      },
+      revalidate: 10
+    };
+  } catch {
+    // different kind of error? -> 404
+    return { notFound: true };
+  }
+};
+
+type Props = {
+  post: {
+    title: string;
+    content: DocumentRendererProps;
+    publishDate: string | null;
+  };
+};
+
+const Blog: NextPage<Props> = ({ post }: Props) => {
+  const getDate = (date: string | null) => {
+    if (!date) return "";
+    const d = new Date(date);
+    return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+  };
+
+  return (
+    <div className="flex flex-col flex-wrap items-center justify-center w-screen h-screen">
+      <Head>
+        <title>blog - meri.garden</title>
+        <meta name="description" content="Meri Leeworthy as writer." />
+      </Head>
+      <header>
+        <h1 className="font-mono text-6xl">{post.title}</h1>
+        <time>{getDate(post.publishDate)}</time>
+      </header>
+      <main className="p-12">
+        <article className="document">
+          <DocumentRenderer document={post.content.document} />
+        </article>
+        <Back />
+      </main>
+    </div>
+  );
+};
+
+export default Blog;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,22 +1,87 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { gql } from "@apollo/client";
+import client from "lib/apollo-client";
 
-const Blog: NextPage = () => {
+const GET_POSTS = gql`
+  query {
+    posts {
+      title
+      slug
+      snippet
+      publishDate
+      author {
+        name
+      }
+    }
+  }
+`;
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const { data, error } = await client.query({
+      query: GET_POSTS
+    });
+    // error -> 404 (rather than just breaking)
+    if (!data || error) return { notFound: true };
+    return {
+      props: {
+        // return modified data
+        posts: data.posts
+      },
+      revalidate: 10
+    };
+  } catch {
+    // different kind of error? -> 404
+    return { notFound: true };
+  }
+};
+
+type Props = {
+  posts: {
+    title: string;
+    slug: string;
+    publishDate: string | null;
+    author: {
+      name: string;
+    };
+    snippet: string;
+  }[];
+};
+
+const Blog: NextPage<Props> = ({ posts }: Props) => {
+  const getDate = (date: string | null) => {
+    if (!date) return "";
+    const d = new Date(date);
+    return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+  };
+
   return (
-    <div>
+    <div className="flex flex-col flex-wrap items-center justify-center w-screen h-screen">
       <Head>
         <title>blog - meri.garden</title>
         <meta name="description" content="Meri Leeworthy as writer." />
       </Head>
-      <main className="flex flex-col flex-wrap items-center justify-center w-screen h-screen">
-        <h2 className="font-mono text-6xl">blog</h2>
-        <p className="max-w-2xl mt-12">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
-          ipsum asperiores nihil quia blanditiis rerum illo? Excepturi atque
-          nulla vitae in, nostrum quasi at neque facere, accusamus provident
-          sapiente minima?
-        </p>
+      <header>
+        <h1 className="font-mono text-6xl">meri blog</h1>
+      </header>
+      <main className="p-12">
+        {posts.map((post) => (
+          <Link key={post.slug} href={`/${post.slug}`}>
+            <a>
+              <article key={post.slug} className="flex max-w-xl space-x-6">
+                <div className="flex flex-col items-end justify-center">
+                  <h2 className="font-mono text-2xl text-right">
+                    {post.title}
+                  </h2>
+                  <time>{getDate(post.publishDate)}</time>
+                </div>
+                <p className="max-w-2xl mt-2 ">{post.snippet}</p>
+              </article>
+            </a>
+          </Link>
+        ))}
       </main>
     </div>
   );
