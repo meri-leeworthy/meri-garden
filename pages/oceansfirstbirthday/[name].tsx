027b80b2d56eeafe7ddd141af2433451655dda1a
Meri Leeworthy <meri.leeworthy@gmail.com>
2022-08-12 04:57:30 +0000

    ocean in flower

--- pages/oceansfirstbirthday/[name].tsx
+++ pages/oceansfirstbirthday/[name].tsx
@@ -1,6 +1,7 @@
 import {useRouter} from 'next/router';
 import Image from "next/image";
 import flower from "public/images/flower.svg";
+import ocean1 from "public/images/ocean1.png";
 
 const Invite = () => {
 	const router = useRouter();
@@ -20,8 +21,15 @@ const Invitation = ({name, message}: {name: string, message: string}) => {
 				<div className="max-w-lg p-4 py-12 text-white">
 					<h1>Hello {name}</h1>
 					<p>oenaoe einsraeiotn  enarstieo neioensart niesranteoi n nsearntei uytasrn tyuny srtyun ayut sryunt yunar tsyunrs tyuny ruystn uyn yunr tsyun  rsyuant yunrsat yu yurasnt yurns tuynrs tyunrs yutn yuasnt yurasnt yunrs tyunrs tyunrs tyunrs tyunsr atyunsra tyunsra tyun</p>
-					<div className="aspect-square w-full relative">
-					<Image alt="flower" layout="fill" src={flower} />
+					<div className="relative w-full aspect-square">
+						<div className="w-full h-full relative">
+							<Image alt="flower" layout="fill" src={flower} />
+						</div>
+						<div className="absolute inset-20 h-full w-full">
+							<div className="relative w-3/5 margin-auto h-2/3">
+								<Image alt="Ocean" layout="fill" src={ocean1} />
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
