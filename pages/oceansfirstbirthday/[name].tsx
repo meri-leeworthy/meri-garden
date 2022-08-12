5507daa1d6ca65ed63b7a6bda6a22560f07da9e0
meri-leeworthy <meri.leeworthy@gmail.com>
2022-08-12 18:49:27 +1000

    done :)

--- pages/oceansfirstbirthday/[name].tsx
+++ pages/oceansfirstbirthday/[name].tsx
@@ -1,4 +1,4 @@
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
 import Image from "next/image";
 import flower from "public/images/flower.svg";
 import ocean1 from "public/images/ocean1.png";
@@ -6,67 +6,110 @@ import ocean2 from "public/images/ocean2.png";
 import ocean3 from "public/images/ocean3.png";
 import oceanH1 from "public/images/oceanH1.svg";
 import oceanH2 from "public/images/oceanH2.svg";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const Invite = () => {
-	const router = useRouter();
-	const {name} = router.query;
-	const stname = name as string;
-	const messages = {
-		ocean: "You are the birthday baby. Of course you are invited!"
-	}
-	// @ts-ignore
-	return <Invitation name={stname} message={messages[stname?.toLowerCase()] || ""} />
-}
+  const router = useRouter();
+  const { name } = router.query;
+  let fmtd = "";
+  let many = false;
+  if (name) {
+    const stname = name as string;
+    const names = stname.split("+");
+    if (names.length > 1) many = true;
+    fmtd = names.reduceRight((fmt, cur, i) => {
+      if (i === names.length - 1)
+        return cur.charAt(0).toLocaleUpperCase() + cur.slice(1);
+      if (i === names.length - 2)
+        return cur[0].toLocaleUpperCase() + cur.slice(1) + " and " + fmt;
+      else return cur[0].toLocaleUpperCase() + cur.slice(1) + ", " + fmt;
+    }, names[names.length - 1].charAt(0).toLocaleUpperCase() + names[names.length - 1].slice(1));
+  }
 
-const Invitation = ({name, message}: {name: string, message: string}) => {
-	const ocean = [ocean1, ocean2, ocean3];
-	const [oceanPic, setOceanPic] = useState(0);
-	useEffect(() => {
-		const interval = setInterval(() => setOceanPic((oceanPic) => (oceanPic + 1) % 3), 3000);
-		return () => {
-			clearInterval(interval);
-		};
-	}, [])
-	return (
-		<div className="w-screen h-full min-h-screen ">
-			<div className="absolute h-full inset-0 bg-[#ff6239] z-30 overflow-y-scroll">
+  return (
+    // @ts-ignore
+    <Invitation name={fmtd} many={many} />
+  );
+};
 
-				<div className="flex justify-center">
-				<div className="max-w-xl w-full p-4 py-12 text-white">
+const Invitation = ({ name, many }: { name: string; many: boolean }) => {
+  const ocean = [ocean1, ocean2, ocean3];
+  const [oceanPic, setOceanPic] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(
+      () => setOceanPic((oceanPic) => (oceanPic + 1) % 3),
+      3000
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <div className="w-screen h-full min-h-screen">
+      <div className="absolute h-full inset-0 bg-[#ff6239] z-30 overflow-y-scroll  overflow-x-hidden">
+        <div className="flex justify-center">
+          <div className="w-full max-w-xl p-4 py-12 text-white">
+            <h1 className="font-[Junicode] mb-8 text-6xl">Dear {name},</h1>
 
-					<h1 className="font-[Junicode] mb-8 text-6xl capitalize">Dear {name},</h1>
+            <div className="relative w-full h-64 -mb-28">
+              <Image
+                alt="Youre invited to Oceans"
+                layout="fill"
+                src={oceanH1}
+              />
+            </div>
 
-					<div className="relative w-full h-64 -mb-28">
-						<Image alt="Youre invited to Oceans" layout="fill" src={oceanH1} />
-					</div>
-					
-					<div className="relative w-full aspect-square">
-						<div className="w-full h-full relative">
-							<Image alt="flower" layout="fill" src={flower} />
-						</div>
-						<div className="absolute inset-20 h-full w-full">
-							<div className="relative w-3/5 margin-auto h-2/3">
-								<Image alt="Ocean" layout="fill" src={ocean[oceanPic]} />
-							</div>
-						</div>
-					</div>
+            <div className="relative w-full aspect-square">
+              <div className="relative w-full h-full">
+                <Image alt="flower" layout="fill" src={flower} />
+              </div>
+              <div className="absolute w-full h-full inset-16 lg:inset-28">
+                <div className="relative w-3/5 margin-auto h-2/3">
+                  <Image alt="Ocean" layout="fill" src={ocean[oceanPic]} />
+                </div>
+              </div>
+            </div>
 
-					<div className="relative w-full h-64 -mt-24">
-						<Image alt="1st birthday party" layout="fill" src={oceanH2} />
-					</div>
+            <div className="relative w-full h-64 -mt-24">
+              <Image alt="1st birthday party" layout="fill" src={oceanH2} />
+            </div>
 
-					<p className="my-4 font-[Junicode] text-2xl">{message}</p>
+            <p className="my-4 font-[Junicode] text-4xl">
+              Please join us for a big party at Edinburgh Gardens Community Room
+              (near the oval) on the 10th of September at 1:30pm
+            </p>
 
-					<p className="my-4 font-[Junicode] text-4xl">
-						Please join us for a big party at Edinburgh Gardens Community Room (near the oval) on the 10th of September at 1:30pm
-					</p>
-					
-				</div>
-			</div>
-			</div>
-		</div>
-	)
-}
+            <p className="my-4 font-[Junicode] text-4xl">
+              If you can make it, please RSVP by clicking here to{" "}
+              <a
+                className="underline"
+                href={`mailto:meri.leeworthy@gmail.com?subject=yes%20${
+                  many ? "we" : "i%27m"
+                }%20coming%20xx%20${name}`}
+              >
+                send us an email
+              </a>{" "}
+              or{" "}
+              <a
+                className="underline"
+                href={`sms:+61426210112?&body=yes%20${
+                  many ? "we" : "i%27m"
+                }%20coming%20xx%20${name}`}
+              >
+                send us a text
+              </a>
+            </p>
+            <p className="my-4 font-[Junicode] text-4xl">
+              Hope to see you there!
+            </p>
+            <p className="my-4 font-[Junicode] text-4xl">
+              Love, Azja and Meri xx
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Invite;
